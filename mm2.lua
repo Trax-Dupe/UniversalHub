70afcc76ec065599c52b079e406a9819e6604961
ScriptsHub11 <martinnader717@gmail.com>
2025-07-21 20:13:32 +0300

    Create mm2.lua

---
+++ mm2.lua
@@ -0,0 +1,5 @@
+-- Disable automatic clipboard copy
+setclipboard = function() end
+
+-- Now call the original script
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptsHub11/Universal/refs/heads/main/mm2.lua"))()
